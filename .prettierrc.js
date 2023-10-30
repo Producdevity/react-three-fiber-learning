a2000ee85370fbe6973f0dca6df09f610b9aec7a
Yassine Gherbi <yassine@hexelnet.com>
2023-10-30 03:59:13 +0100

    chore: inital setup with basic example

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+}
+
+export default config
---
+++ src/components/CanvasWithBoxes.tsx
@@ -0,0 +1,15 @@
+import { Canvas } from '@react-three/fiber'
+import Box from './Box'
+
+function CanvasWithBoxes() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
+
+export default CanvasWithBoxes
